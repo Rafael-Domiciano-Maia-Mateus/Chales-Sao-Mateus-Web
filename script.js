aaec4ae7007a77b9b57a08eb74533d65241c15b6
Rafael-Domiciano-Maia-Mateus <rmaia7985@gmail.com>
2025-08-24 22:19:29 -0300

    Chales Sao Mateus web

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const menu = document.getElementById('menu');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+const imagensFundo = ['img/homeImg/home.png', 'img/homeImg/home2.png', 'img/homeImg/home3.png', 'img/homeImg/home4.png'];
+const homeSection = document.querySelector('.home');
+
+if (homeSection) {
+    imagensFundo.forEach((img, index) => {
+        const div = document.createElement('div');
+        div.classList.add('home-bg');
+        if (index === 0) div.classList.add('active');
+        div.style.backgroundImage = `url(${img})`;
+        homeSection.appendChild(div);
+    });
+
+    const bgDivs = document.querySelectorAll('.home-bg');
+    let i = 0;
+
+    setInterval(() => {
+        bgDivs[i].classList.remove('active');
+        i = (i + 1) % bgDivs.length;
+        bgDivs[i].classList.add('active');
+    }, 5000);
+}
